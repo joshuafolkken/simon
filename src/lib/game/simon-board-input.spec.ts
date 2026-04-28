356204e1d5d41052ba300598fef376d451c8e7bd
joshuafolkken <joshuafolkken@gmail.com>
2026-04-28 22:16:27 +0700

    Stub simon spies in simon-board-input.spec.ts to avoid side-effectful call-through #106

--- src/lib/game/simon-board-input.spec.ts
+++ src/lib/game/simon-board-input.spec.ts
@@ -14,21 +14,21 @@ describe('simon_board_input', () => {
 
 	describe('on_button_pointer_down', () => {
 		it('does not call simon.press when session is not started', () => {
-			const spy = vi.spyOn(simon, 'press');
+			const spy = vi.spyOn(simon, 'press').mockImplementation(() => {});
 			simon_board_input.on_button_pointer_down({ nativeEvent: { button: LEFT_BUTTON } }, 'green');
 			expect(spy).not.toHaveBeenCalled();
 		});
 
 		it('calls simon.press once session has started', () => {
 			session.start_session();
-			const spy = vi.spyOn(simon, 'press');
+			const spy = vi.spyOn(simon, 'press').mockImplementation(() => {});
 			simon_board_input.on_button_pointer_down({ nativeEvent: { button: LEFT_BUTTON } }, 'red');
 			expect(spy).toHaveBeenCalledWith('red');
 		});
 
 		it('does not call simon.press for non-left click even after session started', () => {
 			session.start_session();
-			const spy = vi.spyOn(simon, 'press');
+			const spy = vi.spyOn(simon, 'press').mockImplementation(() => {});
 			simon_board_input.on_button_pointer_down({ nativeEvent: { button: RIGHT_BUTTON } }, 'blue');
 			expect(spy).not.toHaveBeenCalled();
 		});
@@ -36,14 +36,14 @@ describe('simon_board_input', () => {
 
 	describe('on_button_release', () => {
 		it('does not call simon.release when session is not started', () => {
-			const spy = vi.spyOn(simon, 'release');
+			const spy = vi.spyOn(simon, 'release').mockImplementation(() => {});
 			simon_board_input.on_button_release();
 			expect(spy).not.toHaveBeenCalled();
 		});
 
 		it('calls simon.release once session has started', () => {
 			session.start_session();
-			const spy = vi.spyOn(simon, 'release');
+			const spy = vi.spyOn(simon, 'release').mockImplementation(() => {});
 			simon_board_input.on_button_release();
 			expect(spy).toHaveBeenCalledTimes(1);
 		});
@@ -51,14 +51,14 @@ describe('simon_board_input', () => {
 
 	describe('on_center_click', () => {
 		it('does not call simon.start when session is not started', () => {
-			const spy = vi.spyOn(simon, 'start');
+			const spy = vi.spyOn(simon, 'start').mockImplementation(() => {});
 			simon_board_input.on_center_click();
 			expect(spy).not.toHaveBeenCalled();
 		});
 
 		it('calls simon.start once session has started', () => {
 			session.start_session();
-			const spy = vi.spyOn(simon, 'start');
+			const spy = vi.spyOn(simon, 'start').mockImplementation(() => {});
 			simon_board_input.on_center_click();
 			expect(spy).toHaveBeenCalledTimes(1);
 		});

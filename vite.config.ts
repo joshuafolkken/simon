031d35e37659a47ac130690df87e7e927e9cd213
joshuafolkken <joshuafolkken@gmail.com>
2026-04-29 22:21:59 +0700

    Display game title and dynamic version on loading overlay #73

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import pkg from './package.json';
+
+process.env['PUBLIC_APP_VERSION'] = pkg.version;
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],

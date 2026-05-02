2084c9320a0cb84712737a439100877ab46cbd62
joshuafolkken <joshuafolkken@gmail.com>
2026-05-02 09:13:49 +0700

    Convert simon-board-input and cyber-switch-input to callback pattern for threlte-kit readiness #160

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import click_url from '$lib/assets/sound/dragon-studio-distorted-electronic-click-472367.opus';
 	import { loading } from '$lib/game/loading.svelte';
 	import { switch_audio } from '$lib/game/switch-audio';
+	import { simon_board_input } from '$lib/game/simon-board-input';
+	import { cyber_switch_input } from '$lib/game/cyber-switch-input';
+	import { simon } from '$lib/simon/simon.svelte';
+	import { game_state } from '$lib/game/state.svelte';
 	import { messages } from '$lib/messages/en';
 
 	loading.configure({
@@ -13,6 +17,12 @@
 		ready: messages.loading_ready
 	});
 	switch_audio.init(click_url);
+	simon_board_input.configure({
+		on_press: (color) => simon.press(color),
+		on_release: () => simon.release(),
+		on_start: () => simon.start()
+	});
+	cyber_switch_input.configure({ on_toggle: game_state.toggle_alt });
 	loading.set_step('initializing');
 
 	let { children } = $props();
